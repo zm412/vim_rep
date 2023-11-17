b9ce7754ce023971d2d5226808995be37f9b60ee
AltynayTazhbaeva <52510904+zm412@users.noreply.github.com>
2023-11-17 09:46:39 -0500

    Update rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -599,4 +599,6 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+beautiful.useless_gap = 3
+
 -- }}}
